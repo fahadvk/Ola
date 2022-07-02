4c14f357512f2c8e154f7940c9c243ed4dd61046
fahadvillan6 <62593931+fahadvillan6@users.noreply.github.com>
2022-07-02 10:52:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function changeAuto(){
+    document.getElementById("item1").src="auto.png";
+  
+
+    document.getElementById("txt").innerHTML="The all too familiar auto ride,minus the  hassle of waiting and haggling for price Aconvenient way to travel everyday"
+}
+function changeBike()
+{
+    document.getElementById("item1").src="bike.png"
+
+
+    document.getElementById("txt").innerHTML="Hate waiting in the snarling traffic each day?Not anymore as with Ola Bike you can now reduce your travel time by at least 50%.Simply bookabike withatap ofabutton and ride away within minutes and all this at the most affordable rates in town!"
+}
+
+function changePlay()
+{
+    document.getElementById("item1").src="prime-play.png"
+    
+}
+function changeKaali()
+{
+    document.getElementById("item1").src="kaali-peeli.png"
+    changeText()
+} 
+function changeSedan()
+{
+    document.getElementById("item1").src="prime-sedan.png"
+    changeText()
+} 
+function changeStation()
+{
+    document.getElementById("item1").src="prime-suv.png"
+    changeText()
+} 
